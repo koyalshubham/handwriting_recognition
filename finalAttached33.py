59f02cf93d1080bad3ce976a966b18685cec0602
agarwalmanvi <94.manviagarwal@gmail.com>
2019-06-23 23:39:21 +0100

    Add files via upload

--- finalAttached33.py
+++ finalAttached33.py
@@ -5,6 +5,19 @@ import math
 from skimage import filters
 from skimage import morphology
 from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# %matplotlib inline
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.optimizers import SGD
+from keras.layers.normalization import BatchNormalization
+from keras.utils import np_utils
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D
+from keras.layers.advanced_activations import LeakyReLU 
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
 
 
 # used to sort contours from left to right top to bottom
@@ -305,7 +318,7 @@ def convert_img_to_csv():
             for img in tqdm(os.listdir(path)):  # iterate over each image per category
 
                 try:
-                    IMG_SIZE = 28
+                    IMG_SIZE = 100
                     img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)  # convert to array
                     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
                     # print(new_array.shape)
@@ -333,7 +346,7 @@ def convert_img_to_csv():
 
     X_train = []
     for img in X:
-        new_img = np.reshape(img, 784)
+        new_img = np.reshape(img, 10000)
         X_train.append(new_img)
 
     X_train = np.asarray(X_train)
@@ -342,10 +355,191 @@ def convert_img_to_csv():
     print("Completed img to csv")
 
 
+
+
+
+
+def make_training_csv():
+
+    DATADIR = "./training-data"
+
+    CATEGORIES = ["Alef", "Ayin", "Bet", "Dalet", "Gimel", "He", "Het", "Kaf", "Kaf-final", "Lamed", "Mem", "Mem-medial", "Nun-final", "Nun-medial", "Pe", "Pe-final", "Qof", "Resh", "Samekh", "Shin", "Taw", "Tet", "Tsadi-final", "Tsadi-medial", "Waw", "Yod", "Zayin"]
+
+    for category in CATEGORIES:  
+        path = os.path.join(DATADIR,category)  
+        for img in os.listdir(path): 
+            img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+            #plt.imshow(img_array, cmap='gray')  
+            #plt.show()  # display!
+
+            #break 
+        #break 
+
+    IMG_SIZE = 100
+
+    training_data = create_training_data()
+
+    sample_labels = np.zeros(len(training_data))
+    for index,sample in enumerate(training_data):
+        sample_labels[index] = sample[1]
+        
+
+    np.arange(27)
+
+    X = []
+    y = []
+
+    for features,label in training_data:
+        X.append(features)
+        y.append(label)
+
+
+    # to dump all data into a csv for X_train
+
+    X_train = []
+    for img in X:
+        new_img = np.reshape(img, 10000)
+        new_img = new_img.tolist()
+        X_train.append(new_img)
+        
+    import csv
+
+    with open('X.csv', 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerows(X_train)
+
+
+    with open('y.csv', 'w') as outfile:
+        writer = csv.writer(outfile)
+        for label in y:
+            writer.writerow([label])
+
+
+def create_training_data():
+    training_data = []
+    for category in CATEGORIES:  
+
+        path = os.path.join(DATADIR,category)  
+        class_num = CATEGORIES.index(category)  # get the classification  (0 to 26).
+
+        for img in tqdm(os.listdir(path)):  # iterate over each image per category
+            
+            try:
+                
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
+                training_data.append([new_array, class_num])  # add this to our training_data
+                
+            except Exception as e:  # in the interest in keeping the output clean...
+                pass
+            except OSError as e:
+                print("OSErrroBad img most likely", e, os.path.join(path,img))
+            except Exception as e:
+                print("general exception", e, os.path.join(path,img))
+    return training_data
+
+
+
+
+
+def run_model():
+
+    with open('X.csv', 'r') as infile:
+        reader = csv.reader(infile)
+        lines = list(reader)
+
+    X = []
+    for row in lines:
+        new_row = [float(i) for i in row]
+        X.append(new_row)
+        
+    X = np.asarray(X)
+
+    with open('y.csv', 'r') as infile:
+        reader = csv.reader(infile)
+        lines = list(reader)
+        
+    y = []
+    for row in lines:
+        new_row = [float(i) for i in row]
+        y.append(new_row)
+        
+    y = np.asarray(y)
+
+    X_train = X.reshape(X.shape[0], 100, 100, 1)
+    X_train = X_train.astype('float32')
+    X_train/=255
+
+    number_of_classes = 27
+
+    Y_train = np_utils.to_categorical(y, number_of_classes)
+
+
+    # Miniaturized VGG-16 model
+
+    model = Sequential()
+
+    model.add(Conv2D(64, (3, 3), input_shape=(100,100,1), activation='relu', padding='same'))
+    #model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(1, 1)))
+
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    #model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(1, 1)))
+
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+    #model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(1, 1)))
+
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    #model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(1, 1)))
+
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    #model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(1, 1)))
+
+    model.add(Flatten())
+
+    model.add(Dense(512, activation='relu', kernel_regularizer=l2(0.0005), bias_regularizer=l2(0.0005)))
+    model.add(Dropout(0.5))
+    model.add(Dense(512, activation='relu', kernel_regularizer=l2(0.0005), bias_regularizer=l2(0.0005)))
+    model.add(Dropout(0.5))
+    model.add(Dense(27, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01,momentum=0.9), metrics=['accuracy'])
+
+    model.summary()
+
+    model.fit(X_train, Y_train, batch_size=128, epochs=50, verbose=1)
+
+    with open('train.csv', 'r') as infile:
+        reader = csv.reader(infile)
+        lines = list(reader)
+
+    X_test = []
+    for row in lines:
+        new_row = [float(i) for i in row]
+        X_test.append(new_row)
+        
+    X_test = np.asarray(X_test)
+
+    predictions= model.predict(X_test)
+
+    np.savetxt('predictions.txt', predictions, delimiter=',')
+
+
+
+
+
+
 # convert scroll img to character imgs
 convert_scroll_to_imgs()
 # convert imgs to csv
 convert_img_to_csv()
-
+make_training_csv()
 # run model
-
+run_model()
