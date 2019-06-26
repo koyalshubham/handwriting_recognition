5ed26ce738c5a34a1ca09c41bff88a2adaa1fcd8
Shubham Koyal <koyal.shubham1992@gmail.com>
2019-06-26 23:02:01 +0200

    File to install all packages

---
+++ requirments.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get upgrade
+sudo pip install opencv-python
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose -y
+sudo pip install scikit-image
+sudo pip install tqdm
+sudo pip install keras
+sudo apt-get install libhdf5-serial-dev
