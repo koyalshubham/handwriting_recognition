5241500500c8a4d8c074a84021142d8a373da845
Shubham Koyal <koyal.shubham1992@gmail.com>
2019-06-27 16:38:37 +0200

    Update requirments.sh

--- requirments.sh
+++ requirments.sh
@@ -7,3 +7,9 @@ sudo pip install scikit-image
 sudo pip install tqdm
 sudo pip install keras
 sudo apt-get install libhdf5-serial-dev
+sudo pip3 install opencv-python
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose -y
+sudo pip3 install scikit-image
+sudo pip3 install tqdm
+sudo pip3 install keras
+sudo pip install tensorflow==2.0.0-beta1
